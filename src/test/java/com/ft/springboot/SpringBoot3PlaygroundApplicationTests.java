c739d0617c328c427f22393b15bbfd92c675ae41
fatihbguclu <curaat79@gmail.com>
2024-11-17 22:14:28 +0300

    init commit

---
+++ src/test/java/com/ft/springboot/SpringBoot3PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ft.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3PlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
