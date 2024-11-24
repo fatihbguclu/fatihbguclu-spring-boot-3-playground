3e79397210b7422f5f70ade645da9fb3decf2196
fatihbguclu <curaat79@gmail.com>
2024-11-24 21:05:18 +0300

    add model, controller, repository and service for Employee

---
+++ src/main/java/com/ft/springrestsecurity/api/EmployeeController.java
@@ -0,0 +1,47 @@
+package com.ft.springrestsecurity.api;
+
+import com.ft.springrestapi.model.Employee;
+import com.ft.springrestapi.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployee() {
+        return employeeService.getAllEmployee();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeById(@PathVariable Long employeeId) {
+        Optional<Employee> optionalEmployee = employeeService.getEmployeeById(employeeId);
+        if (optionalEmployee.isEmpty()) {
+            throw new RuntimeException("Employee not found with id : " + employeeId);
+        }
+        return optionalEmployee.get();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        return employeeService.updateEmployee(employee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable Long employeeId) {
+        employeeService.deleteEmployeeById(employeeId);
+        return ResponseEntity.ok("Deleted employee with id : " + employeeId);
+    }
+}
---
+++ src/main/java/com/ft/springrestsecurity/model/Employee.java
@@ -0,0 +1,26 @@
+package com.ft.springrestsecurity.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+}
