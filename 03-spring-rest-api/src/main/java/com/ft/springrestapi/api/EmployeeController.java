84f46c3ad7daba633bf6eac547854d5018c464d5
fatihbguclu <curaat79@gmail.com>
2024-11-24 10:25:36 +0300

    add rest endpoint for getting all Employee

--- 03-spring-rest-api/src/main/java/com/ft/springrestapi/api/EmployeeController.java
+++ 03-spring-rest-api/src/main/java/com/ft/springrestapi/api/EmployeeController.java
@@ -1,13 +1,22 @@
 package com.ft.springrestapi.api;
 
+import com.ft.springrestapi.model.Employee;
 import com.ft.springrestapi.service.EmployeeService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class EmployeeController {
 
     private final EmployeeService employeeService;
+
+    @GetMapping("/")
+    public List<Employee> getAllEmployee() {
+        return employeeService.getAllEmployee();
+    }
 
 }
--- 03-spring-rest-api/src/main/java/com/ft/springrestapi/service/EmployeeService.java
+++ 03-spring-rest-api/src/main/java/com/ft/springrestapi/service/EmployeeService.java
@@ -1,8 +1,11 @@
 package com.ft.springrestapi.service;
 
+import com.ft.springrestapi.model.Employee;
 import com.ft.springrestapi.repository.EmployeeRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -10,4 +13,7 @@ public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
+    public List<Employee> getAllEmployee() {
+        return employeeRepository.findAll();
+    }
 }
