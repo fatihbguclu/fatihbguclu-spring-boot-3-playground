9c1eccde970019f080a274cbfd5118afbc232ade
fatihbguclu <curaat79@gmail.com>
2024-11-30 19:13:26 +0300

    add ui pages

--- src/main/java/com/ft/springmvc/controller/CustomerController.java
+++ src/main/java/com/ft/springmvc/controller/CustomerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller
+@Controller("/customer")
 public class CustomerController {
 
     @InitBinder
@@ -21,7 +21,7 @@ public class CustomerController {
         dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
     }
 
-    @GetMapping("/")
+    @GetMapping()
     public String showForm(Model theModel) {
         theModel.addAttribute("customer", new Customer());
         return "customer-form";
