db5a9e545395eb4ffd8f8f0f728209880c6e1dd1
fatihbguclu <curaat79@gmail.com>
2024-12-04 19:41:50 +0300

    add model for One to One mapping example

---
+++ src/main/java/com/ft/springmvchibernateadvanced/model/Instructor.java
@@ -0,0 +1,33 @@
+package com.ft.springmvchibernateadvanced.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "instructor_detail_id")
+    private InstructorDetail instructorDetail;
+
+}
---
+++ src/main/java/com/ft/springmvchibernateadvanced/model/InstructorDetail.java
@@ -0,0 +1,29 @@
+package com.ft.springmvchibernateadvanced.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name="hobby")
+    private String hobby;
+
+    @OneToOne(mappedBy = "instructorDetail", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private Instructor instructor;
+
+}
