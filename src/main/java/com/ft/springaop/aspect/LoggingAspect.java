8fee0f1ae14d3ad513c7872cd8881b75332d3a17
fatihbguclu <curaat79@gmail.com>
2024-12-09 20:09:45 +0300

    usage of JoinPoint

--- src/main/java/com/ft/springaop/aspect/LoggingAspect.java
+++ src/main/java/com/ft/springaop/aspect/LoggingAspect.java
@@ -1,10 +1,14 @@
 package com.ft.springaop.aspect;
 
+import com.ft.springaop.Account;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -12,8 +16,20 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 
     @Before("com.ft.springaop.aspect.AopPointcut.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint joinPoint) {
         System.out.println("\n=====>>> Executing @Before advice on method");
+
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("Method: " + methodSignature);
+
+        Object[] args = joinPoint.getArgs();
+        Arrays.stream(args).forEach(arg -> {
+            System.out.println(arg);
+            if (arg instanceof Account account) {
+                System.out.println("account name: " + account.getName());
+                System.out.println("account level: " + account.getLevel());
+            }
+        });
     }
 
 }
